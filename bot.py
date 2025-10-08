c8334b0edc390f7f8e32e6dff6117bec88304458
AymanPro-cyber <aymanprozq@gmail.com>
2025-10-08 12:57:09 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+print("Hello! I'm AymanBot by AymanX Studios™.")
+
+print("What's your name?")
+uname = input("Ans: ")
+
+print(f"nice to meet you,{uname}")
+
+print("What is your favorite color?")
+fc = input("Ans: ")
+print(f"{fc} is a great color")
+if fc == "blue":
+    print("My creator likes blue,too.")
+    
+elif fc == "Blue":
+    print("My creator likes blue,too.")
+    
+else:
+    print("Do you know my creator likes blue?")
+    
+print("What's my creator's name?")
+cn = input(f"Ans: ")
+
+if cn == "Ayman" or cn == "ayman" or cn == "sawom" or cn == "Sawom":
+    print("Correct!")
+    
+else:  
+    while True:
+        print("W̷̧̡̭̹͖̪̥̖͚̲̠͚͚͖̙͓̤͉͇̠͔̭̫̺̟̝͍͒͊̅̐̀̾̾͊͆̔̊̊͌̈́̍̐̀̓͝͝R̴̨̟̟̪̖͉̮̱̱̗̰̳͔͍̣͓̟͇̺̲̺̞̉̆͛̐̈́͆̑̊̋͘͠ͅO̴̢̨̨͔̜̭̩̙̥̲̣̘̱͈̘̩̝̤͙̩̰̗͇̯̞̻̓̔͒̇̃̀̎͒́͌̂͐̏̽͆͒͂̊̊̚̕͠N̵͓̺͈̲͕̬͕͓̩̞̟̝̻̹̦̦̥̑̀̐̀̿̂͊̎̇̎̏̕̚͝G̸̢̧͉̦͖̝̞̥̎͋̊ ̷̲̩̹̜̥̗̠̖̭̟̼̪̫̜̬̹̺̪͍̮͑͆͌̐͊̔̔͌̅̏̅̈́̏͛̄̅͝͝ͅA̵̧̭̠̰͇͕͍̩̖͓̻̹̤̥̲̹̥͉̯͇̘̯̲̖̪̦̓̾͂̀̇̎͌͌̀́́́Ṅ̷̨̨̠̬͍̝̳̠͖̫̤̺̳͔͔̖̲̟̤̹͉͗͊̋̎̽Š̴̛̘͗̂̈̄͋͆̈́̾́̃̀́̆̚͘͝ͅẄ̶̨̘͕̭̜̣͉̼́E̶̳͇̮͓̦̥̹̗̗̘̰̱̥̮͉͔̥̅̉̊́͆̒̏̈̅͐͌̒̏̚̕͠R̸͚͖̈͗̍̐̔̒͂̃͋̚ ̴̨̛̛̛͔̗̟̠̪͎̜͈͖̥͎̀͗̂̅́̐̂̑̈́́͐̐̾̓͗͂͒̇͘͘̕͜͝Ḅ̵̥͖̲̯͎̩̠͔̻̰̮̤͕̆̒̅͋̀̈̅͆̃͐̔͛̔̽̕͜͝I̶̡͔͔͙̗̥̳̘͙̫̻͂̃̋̅͒̽̑̇͝Ţ̴̺̍̆́͂̌́̃́̌̀Ċ̴̡̺͈͈͚͈̹̝̮͖͎̦̲̺̯͈͙̂̎̂͆̉̎̏̓͑̈́̓̎͌͌͘H̸̢̛̗̯̜̘̱͈̬̰͈̀͗́̊̒̑̆͂͒̇͆̊͒̍̍͒̄͘͝͝")
